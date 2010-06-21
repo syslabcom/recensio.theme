7448410dc642d35866ab3470552f841bc57f3704
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-06-21 11:37:45 +0000

    prepare handling of contenttypes, add line wrap
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@5326 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/pdfgen.py
+++ recensio/theme/browser/pdfgen.py
@@ -9,6 +9,8 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter, A4
 from reportlab.lib.units import cm
 from reportlab.lib.colors import grey
+from reportlab.platypus import Paragraph
+from reportlab.lib.styles import ParagraphStyle
 
 log = logging.getLogger('recensio.theme/pdfgen.py')
 
@@ -20,7 +22,9 @@ class GeneratePdfRecension(BrowserView):
         BrowserView.__init__(self, context, request)
         self.copyright = u"This article may be downloaded and/or used within the private copying\nexemption. Any further use without permission of the rights shall be subject to\nlegal licences (§§ 44a-63a UrhG / German Copyright Act).\n\nDieser Beitrag kann vom Nutzer zu eigenen nicht-kommerziellen Zwecken\nheruntergeladen und/oder ausgedruckt werden. Darüber hinaus gehende\nNutzungen sind ohne weitere Genehmigung der Rechteinhaber nur im Rahmen\nder gesetzlichen Schrankenbestimmungen (§§ 44a-63a UrhG) zulässig."
 
-        self.metadata_template = u"%(rezensionAutor)s, review of: %(autorDesBuchs)s, %(titel)s. %(untertitel)s, \n%(erscheinungsort)s: %(verlag)s %(erscheinungsjahr)s, in: %(reihe)s \nBand %(reihennummer)s, p. %(seitenzahl)s, %(absolute_url)s"
+        self.metadata_template = {
+            "RezensioneinerMonographie": u"%(rezensionAutor)s, review of: %(authors)s, %(title)s%(titel_divider)s%(untertitel)s, \n%(erscheinungsort)s: %(verlag)s %(erscheinungsjahr)s, in: %(reihe)s \nBand %(reihennummer)s, p. %(seitenzahl)s, %(absolute_url)s",
+            }
 
     def __call__(self):
         return self.genPdfRecension()
@@ -32,9 +36,9 @@ class GeneratePdfRecension(BrowserView):
         R.setHeader('content-length', str(contentlength))
 
     def _genCoverSheet(self):
-        metadata_fields = [ 'rezensionAutor', 'autorDesBuchs', 'titel', 'untertitel', 'erscheinungsort', 'verlag', 
-                            'erscheinungsjahr', 'reihe', 'reihennummer', 'seitenzahl',  ]
+        metadata_fields = map(lambda f: f.getName(), self.context.schema.getSchemataFields('default')) # [ 'rezensionAutor', 'autorDesBuchs', 'titel', 'untertitel', 'erscheinungsort', 'verlag', 'erscheinungsjahr', 'reihe', 'reihennummer', 'seitenzahl',  ]
         metadata_dict = dict()
+        metadata_dict['seitenzahl'] = '123-456'
         for field in metadata_fields:
             log.debug('getting field %s' % field)
             metadata_dict[field] = self.context.getField(field).getAccessor(self.context)()
@@ -43,8 +47,12 @@ class GeneratePdfRecension(BrowserView):
                 for val in metadata_dict[field]:
                     strval += val + ', '
                 metadata_dict[field] = strval[:-2]
-            if not isinstance(metadata_dict[field], unicode):
-                metadata_dict[field] = metadata_dict[field].decode('utf8')
+            if metadata_dict[field] and not isinstance(metadata_dict[field], unicode):
+                try:
+                    metadata_dict[field] = metadata_dict[field].decode('utf8')
+                except AttributeError:
+                    log.warn('AttributeError while trying to decode %s (%s)' % (field, metadata_dict[field]))
+        metadata_dict['titel_divider'] = u'. ' if metadata_dict['untertitel'] else u''
         metadata_dict['absolute_url'] = unicode(self.context.absolute_url())
         log.debug(metadata_dict)
 
@@ -63,9 +71,10 @@ class GeneratePdfRecension(BrowserView):
         cover.drawString(2.50*cm, pheight-5.5*cm, u'citation style')
         cover.drawString(2.50*cm, pheight-21.5*cm, u'copyright')
 
-        metadata_txt = cover.beginText(6.20*cm, pheight-6.5*cm)
-        metadata_txt.textLines(self.metadata_template % metadata_dict)
-        cover.drawText(metadata_txt)
+        style = ParagraphStyle('citation style', fontName = 'Helvetica', fontSize = 10, textColor = grey)
+        P = Paragraph(self.metadata_template[self.context.meta_type] % metadata_dict, style)
+        P.wrap(pwidth-6.20*cm-2.5*cm, 10*cm)
+        P.drawOn(cover, 6.20*cm, pheight-6.5*cm)
 
         copyright_txt = cover.beginText(6.20*cm, pheight-22.5*cm)
         copyright_txt.textLines(self.copyright)
