49244a4159075e86f53f204831020158043aa8bc
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-06-22 15:59:03 +0000

    Doh, that doesn't work like that
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@5375 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/interfaces.py
+++ recensio/theme/browser/interfaces.py
@@ -1,18 +1,10 @@
 from zope.interface import Interface
 from plone.theme.interfaces import IDefaultPloneLayer
 
-class IThemeSpecific(IDefaultPloneLayer):
-    """Marker interface that defines a Zope 3 browser layer.
-    """
-
-class IRecensioLayer(Interface):
-    """Marker Interface for a custom BrowserLayer
-    """
-
 class IGeneratePdfRecension(Interface):
     """Interface for PDF generation BrowserView
     """
-    
+
     def genPdfRecension(self):
         """Generate and return a PDF version of the recension
         """
