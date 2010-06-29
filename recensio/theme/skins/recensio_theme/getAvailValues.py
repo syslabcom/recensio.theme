fe81b693d9e58ffa20914ab5efaba34b75639bf3
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-06-29 14:25:10 +0000

    handle hierarchical vocabularies; some renaming
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@5509 51ee9ba4-e84d-4896-84de-354f1821c169

---
+++ recensio/theme/skins/recensio_theme/getAvailValues.py
@@ -0,0 +1,20 @@
+## Script (Python) "getAvailValues"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=voctype
+##title=Returns available values for ddcPlace
+##
+outlines = []
+for line in context.portal_vocabularies.getVocabularyByName(voctype).getVocabularyLines():
+    voclbl = line[1][0]
+    vocval = line[0]
+    outlines.append(dict(label=voclbl, value=vocval))
+    for subline in line[1][1].items():
+        subvoclbl = subline[1][0]
+        subvocval = subline[0]
+        outlines.append(dict(label='%s - %s' % (voclbl,subvoclbl), value=subvocval))
+
+return outlines
