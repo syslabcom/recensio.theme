a62d2120752633214512d9bc55bf89d2ca2c86b5
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-08-06 11:38:10 +0000

    added editors to output of listAuthors
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@5879 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/skins/recensio_theme/listAuthors.py
+++ recensio/theme/skins/recensio_theme/listAuthors.py
@@ -4,11 +4,18 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=
+##parameters=listEditors=False
 ##title=Returns a list of authors in the format "firstname lastname"
 ##
+if not getattr(context, 'getAuthors', None):
+    return None
 authors_list = []
 for author in context.getAuthors():
     authors_list.append(u'%s %s' % (author['firstname'].decode('utf8'), author['lastname'].decode('utf8')))
+if listEditors:
+    if not getattr(context, 'getEditorsCollectedEdition', None):
+        return authors_list
+    for editor in context.getEditorsCollectedEdition():
+        authors_list.append(u'%s %s' % (editor['firstname'].decode('utf8'), editor['lastname'].decode('utf8')))
 
 return authors_list
