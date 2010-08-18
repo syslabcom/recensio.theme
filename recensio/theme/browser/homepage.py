bce93cf569f8c5920f0a07dbb7b2e8777e5789e4
thomas <thomas@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-08-18 11:43:04 +0000

    dynamic displays for the homepage; only functional, still butt-ugly
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@5913 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/homepage.py
+++ recensio/theme/browser/homepage.py
@@ -10,3 +10,63 @@ class HomepageView(BrowserView):
 
     def __call__(self):
         return self.template(self)
+
+    def getReviewMonographs(self):
+        pc = getToolByName(self.context, 'portal_catalog')
+        plt = getToolByName(self.context, 'portal_languages')
+        langinfo = plt.getAvailableLanguageInformation()
+        # import pdb; pdb.set_trace()
+        query = dict(portal_type="Review Monograph",
+            Language='all',
+            sort_on='created',
+            sort_order='reverse')
+        resultset = list()
+        for lang in ('de', 'en', 'fr'):
+            q = query.copy()
+            q['languageReview'] = lang
+            res = pc(q)
+            resultset.append(dict(language=lang,
+                langname=langinfo[lang]['native'],
+                results=res[:5]))
+
+        return resultset
+
+    def getPrintedPresentations(self):
+        pc = getToolByName(self.context, 'portal_catalog')
+        query = dict(portal_type=('Presentation Article Review',
+                'Presentation Monograph', 'Presentation Collection'),
+            Language='all',
+            sort_on='created',
+            sort_order='reverse')
+        res = pc(query)
+        return res[:5]
+
+    def getOnlinePresentations(self):
+        pc = getToolByName(self.context, 'portal_catalog')
+        query = dict(portal_type='Presentation Online Resource',
+            Language='all',
+            sort_on='created',
+            sort_order='reverse')
+        res = pc(query)
+        return res[:5]
+
+    def getReviewJournals(self):
+        pc = getToolByName(self.context, 'portal_catalog')
+        query = dict(portal_type='Review Journal',
+            Language='all',
+            sort_on='created',
+            sort_order='reverse')
+        res = pc(query)
+        resultset = list()
+        for r in res[:5]:
+            try:
+                o = r.getObject()
+            except:
+                # XXX log error here
+                continue
+            publication = o.get_publication_object()
+            publication_title = publication and publication.Title() or u''
+            resultset.append(dict(Title=r.Title, url=r.getURL(),
+                publication_title=publication_title,
+                ))
+        return resultset
