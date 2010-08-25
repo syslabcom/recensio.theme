322868da7f3119c319abbcc9f7fbf2bb41788ed6
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-08-25 14:21:58 +0000

    Good practise added
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@6054 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/skins/recensio_theme/javascript.js
+++ recensio/theme/skins/recensio_theme/javascript.js
@@ -1,5 +1,7 @@
 // hide_title is a style for the rich text editor.
 // This logic will always hide/unhide the next element
-jq('h4.hide_title').click(function(){
-    jq(this).next().toggle();
+jq(function(){
+    jq('h4.hide_title').click(function(){
+        jq(this).next().toggle();
+    });
 });
