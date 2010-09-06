5c251fb43dbce828f2b76b50e8f0f94bd480138f
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-09-06 13:29:42 +0000

    moved util functions to recensio.policy.utility, fixed display of selected facets
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@6330 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/topical.py
+++ recensio/theme/browser/topical.py
@@ -1,61 +1,18 @@
 import logging
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
-from collective.solr.browser.facets import convertFacets, SearchFacetsView
+from collective.solr.browser.facets import SearchFacetsView
 from Products.Archetypes.utils import OrderedDict
 from zope.component import queryUtility
 from collective.solr.interfaces import ISolrConnectionConfig
-from collective.solr.browser.facets import param, facetParameters
+from collective.solr.browser.facets import param
 from copy import deepcopy
 from ZTUtils import make_query
 from recensio.contenttypes.config import PORTAL_TYPES
+from recensio.policy.utility import getSelectedQuery, \
+    convertFacets, browsing_facets as facet_fields
 
 log = logging.getLogger('recensio.theme/topical.py')
-
-facet_fields = ['ddcPlace', 'ddcTime', 'ddcSubject']
-forbidden_types = ('Topic', 'Folder', 'Document', 'Image', 'Issue', 'Publication', 'SimpleVocabulary', 'SimpleVocabularyTerm', 'TreeVocabulary', 'TreeVocabularyTerm', 'Volume')
-
-def facetParameters(context, request):
-    """ determine facet fields to be queried for """
-    fields = facet_fields
-    dependencies = {}
-    return fields, dependencies
-
-def convertFacets(fields, context=None, request={}, filter=None):
-    """ convert facet info to a form easy to process in templates """
-    info = []
-    params = request.copy()   # request needs to be a dict, i.e. request.form
-    facets, dependencies = list(facetParameters(context, request))
-    params['facet.field'] = facets = list(facets)
-    fq = params.get('fq', [])
-    if isinstance(fq, basestring):
-        fq = params['fq'] = [fq]
-    selected = set([facet.split(':', 1)[0].strip('+') for facet in fq ])
-    selected = selected.intersection(set(facet_fields))
-    for field, values in fields.items():
-        counts = []
-        second = lambda a, b: cmp(b[1], a[1])
-        for name, count in sorted(values.items(), cmp=second):
-            p = deepcopy(params)
-            p.setdefault('fq', []).append('%s:"%s"' % (field, name.encode('utf-8')))
-            if filter is None or filter(name, count):
-                counts.append(dict(name=name, count=count,
-                    query=make_query(p, doseq=True)))
-        deps = dependencies.get(field, None)
-        visible = deps is None or selected.intersection(deps)
-        if counts and visible:
-            info.append(dict(title=field, counts=counts))
-    if facets:          # sort according to given facets (if available)
-        def pos(item):
-            try:
-                return facets.index(item)
-            except ValueError:
-                return len(facets)      # position the item at the end
-        func = lambda a, b: cmp(pos(a), pos(b))
-    else:               # otherwise sort by title
-        func = lambda a, b: cmp(a['title'], b['title'])
-    return sorted(info, cmp=func)
-
 
 class BrowseTopicsView(SearchFacetsView):
     """View for topical browsing (ddcPlace etc.)
@@ -213,14 +170,9 @@ class BrowseTopicsView(SearchFacetsView):
         for submenu in submenus:
             mid = submenu['id']
             cq = [item for item in menu[mid] if item.has_key('clearquery')]
-            if len(cq)==1:
-                clearquery = "%s?%s" % (self.request['ACTUAL_URL'], cq[0]['clearquery'])
-                voc = cq[0]['voc']
-            else: 
-                clearquery = ''
-                voc = ''
-            submenu['clearquery'] = clearquery
-            submenu['voc'] = voc
+            for item in menu[mid]:
+                cq = cq + [subitem for subitem in item['submenu'] if subitem.has_key('clearquery')]
+            submenu['selected'] = cq
         
         return submenus
         
