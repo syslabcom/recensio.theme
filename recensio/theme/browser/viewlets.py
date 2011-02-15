7f56bd22b4a057d12a8dbe5a3d81432648e5e1cf
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-02-15 13:07:08 +0000

    Rewrite of the publicationslisting viewlet. Fixes #2618
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@7739 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/viewlets.py
+++ recensio/theme/browser/viewlets.py
@@ -1,10 +1,15 @@
-from AccessControl.unauthorized import Unauthorized
+""" Viewlets for Recensio.net
+
+ * publicationlisting viewlet
+
+"""
+
 from zope.interface import implements
 from zope.viewlet.interfaces import IViewlet
-from Products.CMFCore.utils import getToolByName
 from plone.app.layout.viewlets import ViewletBase
 
 class publicationlisting(ViewletBase):
+    """ Lists Volumes/Issues/Reviews in the current Publication"""
     implements(IViewlet)
 
     def visible(self):
@@ -24,40 +29,60 @@ class publicationlisting(ViewletBase):
         return False
 
     def volumes(self):
-        """ return a mapping of all volumes including contained issues
-        and reviews """
-        try:
-            parent = self.request.PARENTS[1]
-        except (IndexError, AttributeError):
-            parent = None
-        if not hasattr(parent, 'getFolderContents'):
-            return []
-        def sortedObjectValues(container, *portal_types):
-            if container == None:
-                return []
-            else:
-                try:
-                    retval = [x.getObject() for x in container.getFolderContents(
-                            contentFilter={'portal_type':portal_types},
-                            full_objects=False)
-                              if x is not None]
-                    retval.sort(lambda a, b: b.effective().__cmp__(a.effective()))
-                    return retval
-                except Unauthorized:
-                    return []
+        """ Return a tree of Reviews for the current publication
 
-        volumes = []
-        for volume in sortedObjectValues(parent, 'Volume'):
-            issues = []
-            for issue in sortedObjectValues(volume, 'Issue'):
-                issuechildren = sortedObjectValues(issue, 'Review Journal',
-                                                   'Review Monograph')
-                issues.append(dict(title=issue.Title(), children=issuechildren))
+        Reviews are contained within a tree of Volumes and Issues
+        under each Publication
+         * Volumes may contain Issues and Reviews
+         * Issues may only contain Reviews
 
-            volumechildren = dict(issues=issues,
-                                  reviews=sortedObjectValues(volume,
-                                                             'Review Journal',
-                                                             'Review Monograph')
-                                  )
-            volumes.append(dict(title=volume.Title(), children=volumechildren))
+        {"vol_1" : {
+                "title" : "Volume 1",
+                "reviews" : [
+                    review_obj_1, review_obj_2
+                    ],
+                "issues" : {
+                    "issue_1": {
+                        "title" : "Issue 1",
+                        "reviews" : [
+                            review_obj_1, review_obj_2
+                            ]}
+                    }
+         }
+         """
+
+        catalog = self.context.portal_catalog
+        path = '/'.join(self.context.getPhysicalPath()[:-1])
+        query = {}
+        query["path"] = {"query" : path}
+        query["portal_type"] = ("Review Journal",
+                                "Review Monograph")
+        query["sort_on"] = "effective"
+        reviews = catalog(query)
+
+        volumes = {}
+        for review in reviews:
+            review_obj = review.getObject()
+            review_parent = review_obj.aq_parent
+            if review_parent.portal_type == "Issue":
+                # The parent must be a Volume
+                volume_obj = review_parent.aq_parent
+                if not volumes.has_key(volume_obj.id):
+                    volumes[volume_obj.id] = { "Title" : volume_obj.title }
+                volume = volumes[volume_obj.id]
+                if not volume.has_key("issues"):
+                    volume["issues"] = {}
+                issues = volume["issues"]
+                if not issues.has_key(review_parent.id):
+                    issues[review_parent.id] = { "Title" : review_parent.title }
+                issue = issues[review_parent.id]
+                issue.setdefault("reviews", []).append(review_obj)
+
+            elif review_parent.portal_type == "Volume":
+                if not volumes.has_key(review_parent.id):
+                    volumes[review_parent.id] = {
+                        "Title" : review_parent.title }
+                volume = volumes[review_parent.id]
+                volume.setdefault("reviews", []).append(review_obj)
+
         return volumes
