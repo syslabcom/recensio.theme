e3b48651c75182f4a302748d7e1ec839aa05a384
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-07-26 15:13:06 +0000

    use faceting to show or hide icons in authorsearch
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@8685 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/authorsearch.py
+++ recensio/theme/browser/authorsearch.py
@@ -2,6 +2,8 @@ from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 
+PRESENTATION_TYPES = ['Presentation Monograph', 'Presentation Online Resource', 'Presentation Article Review', 'Presentation Collection']
+REVIEW_TYPES = ['Review Journal', 'Review Monograph']
 
 class AuthorSearchView(BrowserView):
     """ Dynamic elements on the homepage """
@@ -16,12 +18,29 @@ class AuthorSearchView(BrowserView):
     def authors(self):
         if not hasattr(self, '_authors'):
             catalog = getToolByName(self.context, 'portal_catalog')
+            reviews = catalog({'fq': '+portal_type:(' + ' OR '.join(map(lambda x: '"%s"' % x, REVIEW_TYPES)) + ')',
+                               'facet': 'true',
+                               'facet.field': 'authors',
+                               'facet.limit': '-1',
+                               'facet.mincount': '1'}).facet_counts['facet_fields']['authors']
+            presentations = catalog({'fq': '+portal_type:(' + ' OR '.join(map(lambda x: '"%s"' % x, PRESENTATION_TYPES)) + ')',
+                                     'facet': 'true',
+                                     'facet.field': 'authors',
+                                     'facet.limit': '-1',
+                                     'facet.mincount': '1'}).facet_counts['facet_fields']['authors']
+            comments = catalog({'fq': '+portal_type:"Discussion Item"',
+                                'facet': 'true',
+                                'facet.field': 'authors',
+                                'facet.limit': '-1',
+                                'facet.mincount': '1'}).facet_counts['facet_fields']['authors']
+
             authors = self.request.get('authors')
             if authors:
                 items = [x.lower() for x in authors.split(' ')]
-                self._authors = [x for x in
+                self._authors = [dict(name=x, reviews=reviews.get(x.decode('utf-8'), 0), presentations=presentations.get(x.decode('utf-8'), 0), comments=comments.get(x.decode('utf-8'), 0)) for x in
                     catalog.uniqueValuesFor('authors') if True in
                     [y in x.lower() for y in items]]
             else:
-                self._authors = catalog.uniqueValuesFor('authors')
+                self._authors = [dict(name=x, reviews=reviews.get(x.decode('utf-8'), 0), presentations=presentations.get(x.decode('utf-8'), 0), comments=comments.get(x.decode('utf-8'), 0)) for x in
+                    catalog.uniqueValuesFor('authors')]
         return self._authors
