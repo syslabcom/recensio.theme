896de81ae83c11cb0db8265b532d0d147a3275ea
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-09-05 11:10:59 +0000

    Removing unused imports and tidy up
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9105 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/views.py
+++ recensio/theme/browser/views.py
@@ -1,7 +1,4 @@
 from Products.Five.browser import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.CMFCore.utils import getToolByName
-from zope.publisher.interfaces import Unauthorized
 
 
 class CreateNewPresentationView(BrowserView):
@@ -12,7 +9,9 @@ class CreateNewPresentationView(BrowserView):
         homefolder = self.context.portal_membership.getHomeFolder()
         if homefolder is None:
             return self.request.RESPONSE.redirect('login_form')
-        return self.request.RESPONSE.redirect(membersfolder.absolute_url()+'/add_new_item')
+        return self.request.RESPONSE.redirect(
+            membersfolder.absolute_url()+'/add_new_item')
+
 
 class ManageMyPresentationsView(BrowserView):
     """ Helper to direct to my presentations """
