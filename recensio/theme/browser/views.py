8ec0cdff3be4a22a0c27a45abbd0394d9715544b
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-09-06 16:18:35 +0000

    Adding some generally useful functions for listing languages to recensio.theme
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9156 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/views.py
+++ recensio/theme/browser/views.py
@@ -1,8 +1,35 @@
-from Products.Five.browser import BrowserView
+""" Views and functions for Recensio.net
+"""
 from zope.app.component.hooks import getSite
+from zope.component import queryUtility
 from zope.interface import implements
 
+from Products.Archetypes.utils import DisplayList
+from Products.Five.browser import BrowserView
+from plone.i18n.locales.languages import _languagelist
+from plone.registry.interfaces import IRegistry
+
+from recensio.policy.interfaces import IRecensioSettings
+
 from interfaces import IRecensioHelperView
+
+
+def listRecensioSupportedLanguages():
+    portal = getSite()
+    vocab = portal.portal_languages.listSupportedLanguages()
+    terms = [(x[0], _languagelist[x[0]][u'native']) for x in vocab]
+    return DisplayList(terms)
+
+def listAvailableContentLanguages():
+    registry = queryUtility(IRegistry)
+    settings = registry.forInterface(IRecensioSettings)
+    allowed_langs = getattr(
+        settings, 'available_content_languages', '').split('\n')
+    terms = []
+    if allowed_langs != [u""]:
+        terms = [(x, _languagelist[x][u'native'])
+                 for x in allowed_langs]
+    return DisplayList(terms)
 
 
 class RecensioHelperView(BrowserView):
@@ -24,6 +51,12 @@ class RecensioHelperView(BrowserView):
             itemtype = "heading_add_%s_title" %fti.content_meta_type
 
         return itemtype
+
+    def listRecensioSupportedLanguages(self):
+        return listRecensioSupportedLanguages()
+
+    def listAvailableContentLanguages(self):
+        return listAvailableContentLanguages()
 
 
 class CreateNewPresentationView(BrowserView):
