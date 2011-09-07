bc12d7c8c9f1bcf94d8282ca365abf66d37bec4d
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-09-07 16:07:17 +0000

    use build_query when called on a topic. refs #3125
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9191 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/filtersearch.py
+++ recensio/theme/browser/filtersearch.py
@@ -1,11 +1,14 @@
 from zope.component import queryUtility
 from Products.Five.browser import BrowserView
 from Products.Archetypes.utils import DisplayList
+from Products.ATContentTypes.interfaces import IATTopic
+
 from plone.registry.interfaces import IRegistry
-from topical import BrowseTopicsView
+
 from recensio.policy.utility import filter_facets
 from recensio.policy.interfaces import IRecensioSettings
 from recensio.theme.browser.views import listAvailableContentLanguages
+from topical import BrowseTopicsView
 
 PORTAL_TYPES = ['Presentation Online Resource', 'Presentation Article Review',
     'Presentation Collection', 'Presentation Monograph',
@@ -20,6 +23,9 @@ class FilterSearchView(BrowseTopicsView):
         self.default_query = {'portal_type': PORTAL_TYPES,
                               'facet': 'true',
                               'facet.field': self.facet_fields, }
+
+        if IATTopic.providedBy(context):
+            self.default_query.update(context.buildQuery())
 
         #self.vocDict = {'languageReviewedText': listAvailableContentLanguages()}
         registry = queryUtility(IRegistry)
