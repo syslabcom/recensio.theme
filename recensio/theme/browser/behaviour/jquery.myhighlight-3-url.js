9fa5c6e923e883407284fce27b4b45239f09f27b
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-09-08 10:35:52 +0000

    highlight script for making urls clickable, refs #3108
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9200 51ee9ba4-e84d-4896-84de-354f1821c169

---
+++ recensio/theme/browser/behaviour/jquery.myhighlight-3-url.js
@@ -0,0 +1,58 @@
+/*
+
+highlight v3-url
+Modified by Marshal (beatgates@gmail.com) to add regexp highlight, 2011-6-24
+Modified by Manuel Reinhardt to insert anchors instead of spans, 2011-9-8
+
+Highlights arbitrary terms. In this version only really makes sense when using URL patterns.
+
+<http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html>
+
+MIT license.
+
+Johann Burkard
+<http://johannburkard.de>
+<mailto:jb@eaio.com>
+
+*/
+
+jQuery.fn.highlight = function(pattern) {
+    var regex = typeof(pattern) === "string" ? new RegExp(pattern, "i") : pattern; // assume very LOOSELY pattern is regexp if not string
+    function innerHighlight(node, pattern) {
+        var skip = 0;
+        if (node.nodeType === 3) { // 3 - Text node
+            var pos = node.data.search(regex);
+            if (pos >= 0 && node.data.length > 0) { // .* matching "" causes infinite loop
+                var match = node.data.match(regex); // get the match(es), but we would only handle the 1st one, hence /g is not recommended
+                var spanNode = document.createElement('a');
+                var middleBit = node.splitText(pos); // split to 2 nodes, node contains the pre-pos text, middleBit has the post-pos
+                var endBit = middleBit.splitText(match[0].length); // similarly split middleBit to 2 nodes
+                var middleClone = middleBit.cloneNode(true);
+                spanNode.appendChild(middleClone);
+                spanNode.href = middleClone.nodeValue; // set href
+                // parentNode ie. node, now has 3 nodes by 2 splitText()s, replace the middle with the highlighted spanNode:
+                middleBit.parentNode.replaceChild(spanNode, middleBit); 
+                skip = 1; // skip this middleBit, but still need to check endBit
+            }
+        } else if (node.nodeType === 1 && node.childNodes && !/(script|style|a)/i.test(node.tagName)) { // 1 - Element node
+            for (var i = 0; i < node.childNodes.length; i++) { // highlight all children
+                i += innerHighlight(node.childNodes[i], pattern); // skip highlighted ones
+            }
+        }
+        return skip;
+    }
+    
+    return this.each(function() {
+        innerHighlight(this, pattern);
+    });
+};
+
+jQuery.fn.removeHighlight = function() {
+    return this.find("span.highlight").each(function() {
+        this.parentNode.firstChild.nodeName;
+        with (this.parentNode) {
+            replaceChild(this.firstChild, this);
+            normalize();
+        }
+    }).end();
+};
