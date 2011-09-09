21eee91b7d57f65780facaf31fc65bf5b06c6f80
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-09-09 13:57:49 +0000

    bugfix use "jq" instead "$"
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9247 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/javascript/homepage.js
+++ recensio/theme/browser/javascript/homepage.js
@@ -1,6 +1,6 @@
-jq(document).ready(function ($) {
-    $("#content_left .ellipsis_container").ThreeDots({max_rows: 3});
-    $('#list_reviews_en').easyticker();
-    $('#list_reviews_de').easyticker();
-    $('#list_reviews_int').easyticker();
-})(jq);
+jq(document).ready(function () {
+    jq("#content_left .ellipsis_container").ThreeDots({max_rows: 3});
+    jq('#list_reviews_en').easyticker();
+    jq('#list_reviews_de').easyticker();
+    jq('#list_reviews_int').easyticker();
+});
