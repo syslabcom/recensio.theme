109082b9e395850acaa573aef0b6f64720cd0d00
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-09-15 15:23:29 +0000

    removed metadata viewlet, this is now handled via COinS the content template
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9312 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/viewlets.py
+++ recensio/theme/browser/viewlets.py
@@ -112,16 +112,3 @@ class publicationlisting(ViewletBase):
                                  key=lambda x: x.get("effective", ""),
                                  reverse=True)
         return sorted_volumes
-
-ADDITIONAL_METADATA = {
-    'publisher'         : 'DC.publisher'
-    }
-
-class AdditionalMetadata(ViewletBase):
-    """ Expose more metadata in Dublin Core style """
-    def getMetadata(self):
-        result = {}
-        for accessor, key in ADDITIONAL_METADATA.items():
-            if accessor in getattr(self.context, 'schema', {}):
-                result[key] = self.context[accessor]
-        return result.items()
