2972e448d65fb278b0d3a5767692669784d6e2e8
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-09-20 16:01:42 +0000

    Fixing the homepage ticker and ThreeDots integration refs #2635
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9385 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/javascript/homepage.js
+++ recensio/theme/browser/javascript/homepage.js
@@ -1,6 +1,18 @@
 jq(document).ready(function () {
-    jq("#content_left .ellipsis_container").ThreeDots({max_rows: 3});
+    jq("#latest_reviewparts_box .ellipsis_container, \
+        #latest_monographies_box .ellipsis_container, \
+        #latest_internet_resources_box .ellipsis_container")
+        .ThreeDots({max_rows: 4});
+    jq("#latest_reviews_box .ellipsis_container").ThreeDots({max_rows: 4});
+    /* The ticker items need to be displayed so that the size can be
+       calculated by ThreeDots. They can then be hidden. Using
+       visibility in css to avoid seeing an ugly flash of text before
+       the page is loaded and the JavaScript kicks in. */
+    jq('div#latest_reviews_box ul li.not_first').css('display', 'none')
+    jq('div#latest_reviews_box ul li.not_first').css('visibility', 'visible')
+
     jq('#list_reviews_en').easyticker();
     jq('#list_reviews_de').easyticker();
     jq('#list_reviews_int').easyticker();
+
 });
