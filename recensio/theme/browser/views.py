65a99f5700f47c79d32d1f7b1d1b6d58c994030e
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-09-21 00:22:42 +0000

    Adding new editorial field for Reviews/Presentations of monographs refs #3111
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9395 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/views.py
+++ recensio/theme/browser/views.py
@@ -2,6 +2,8 @@
 """
 from zope.app.component.hooks import getSite
 from zope.component import queryUtility
+from zope.i18n import translate
+from zope.i18nmessageid import Message
 from zope.interface import implements
 
 from Products.Archetypes.utils import DisplayList
@@ -9,9 +11,11 @@ from Products.Five.browser import BrowserView
 from plone.i18n.locales.languages import _languagelist
 from plone.registry.interfaces import IRegistry
 
+from recensio.contenttypes import contenttypesMessageFactory as _
 from recensio.policy.interfaces import IRecensioSettings
 
 from interfaces import IRecensioHelperView
+
 
 
 def listRecensioSupportedLanguages():
@@ -24,13 +28,26 @@ def listAvailableContentLanguages():
     registry = queryUtility(IRegistry)
     settings = registry.forInterface(IRecensioSettings)
     allowed_langs = getattr(
-        settings, 'available_content_languages', '').replace('\r', '').split('\n')
+        settings, 'available_content_languages', ''
+        ).replace('\r', '').split('\n')
     terms = []
     if allowed_langs != [u""]:
         terms = [(x, _languagelist[x][u'native'])
                  for x in allowed_langs]
     return DisplayList(terms)
 
+def recensioTranslate(msgid):
+    portal = getSite()
+    language = portal.portal_languages.getPreferredLanguage()
+    return translate(Message(msgid, domain="recensio"),
+                     target_language=language)
+
+def editorTypes():
+    return DisplayList((
+            ("herausgeber", recensioTranslate(u"label_abbrev_herausgeber"),),
+            ("bearbeiter", recensioTranslate(u"label_abbrev_bearbeiter"),),
+            ("redaktion", recensioTranslate(u"label_abbrev_redaktion"),),
+            ))
 
 class RecensioHelperView(BrowserView):
     """ General purpose view methods for Recensio """
