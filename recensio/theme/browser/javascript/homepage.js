aae8f810ee981d6b7ee8a9a726179db3c58efea0
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-09-23 10:26:56 +0000

    tightened screws on ellipsis
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9424 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/javascript/homepage.js
+++ recensio/theme/browser/javascript/homepage.js
@@ -3,7 +3,7 @@ jq(document).ready(function () {
         #latest_monographies_box .ellipsis_container, \
         #latest_internet_resources_box .ellipsis_container")
         .ThreeDots({max_rows: 4});
-    jq("#latest_reviews_box .ellipsis_container").ThreeDots({max_rows: 4});
+    jq("#latest_reviews_box .ellipsis_container").ThreeDots({max_rows: 3});
     /* The ticker items need to be displayed so that the size can be
        calculated by ThreeDots. They can then be hidden. Using
        visibility in css to avoid seeing an ugly flash of text before
