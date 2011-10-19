e4dbc80779f738946aeda61581adbd86fdee27cf
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-10-19 11:39:06 +0000

    workaround for IE toggle problem, refs #3107
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9560 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/skins/recensio_theme/javascript.js
+++ recensio/theme/skins/recensio_theme/javascript.js
@@ -1,9 +1,13 @@
 jq(document).ready(function() {
 // hide_title is a style for the rich text editor.
 // This logic will always hide/unhide the next element
-    jq('h4.hide_title').click(function() {
-        jq(this).next().toggle();
-    });
+    if (navigator.appName.search("Microsoft") !== -1) { // workaround for IE
+        jq('p.hide_content').show();
+    } else {
+        jq('h4.hide_title').click(function() {
+            jq(this).next().toggle();
+        });
+    }
 
     jq('.easyticker').easyticker({
         speed: 'slow',
