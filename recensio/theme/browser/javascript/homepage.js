8203a7fd6d3b850e84067c24a6b88aeda902c118
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-10-20 14:17:04 +0000

    Ensuring that the maximum number of lines is the same for each area on the home page refs #2627
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9585 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/javascript/homepage.js
+++ recensio/theme/browser/javascript/homepage.js
@@ -1,9 +1,8 @@
 jq(document).ready(function () {
     jq("#latest_reviewparts_box .ellipsis_container, \
         #latest_monographies_box .ellipsis_container, \
-        #latest_internet_resources_box .ellipsis_container")
-        .ThreeDots({max_rows: 4});
-    jq("#latest_reviews_box .ellipsis_container").ThreeDots({max_rows: 3});
+        #latest_internet_resources_box .ellipsis_container, \
+        #latest_reviews_box .ellipsis_container").ThreeDots({max_rows: 3});
     /* The ticker items need to be displayed so that the size can be
        calculated by ThreeDots. They can then be hidden. Using
        visibility in css to avoid seeing an ugly flash of text before
