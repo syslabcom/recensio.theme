bdf78eb3b4eaeec56d3771296be4ec2a64769480
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-10-25 09:33:22 +0000

    Allow only a maximum of two rows in the ticker area "latest_reviews_box", this is to prevent the content overflowing when the combination of a long author name and a long first word in the title causes the whole area to overflow by one. An alternative solution would be to include the author name as part of the link text.
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9620 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/javascript/homepage.js
+++ recensio/theme/browser/javascript/homepage.js
@@ -3,6 +3,7 @@ jq(document).ready(function () {
         #latest_monographies_box .ellipsis_container, \
         #latest_internet_resources_box .ellipsis_container, \
         #latest_reviews_box .ellipsis_container").ThreeDots({max_rows: 3});
+    jq("#latest_reviews_box .ellipsis_container").ThreeDots({max_rows: 2});
     /* The ticker items need to be displayed so that the size can be
        calculated by ThreeDots. They can then be hidden. Using
        visibility in css to avoid seeing an ugly flash of text before
