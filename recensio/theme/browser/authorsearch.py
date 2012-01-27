5b1698d1bd7aac11d04bbd4b703b6810ae7b984b
reinhardt <reinhardt@51ee9ba4-e84d-4896-84de-354f1821c169>
2012-01-27 14:08:31 +0000

    cache authors and alphabet index
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@9829 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/browser/authorsearch.py
+++ recensio/theme/browser/authorsearch.py
@@ -2,6 +2,8 @@ from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import safe_unicode
+from plone.memoize import ram
+from DateTime import DateTime
 
 PRESENTATION_TYPES = ['Presentation Monograph', 'Presentation Online Resource', 'Presentation Article Review', 'Presentation Collection']
 REVIEW_TYPES = ['Review Journal', 'Review Monograph']
@@ -16,45 +18,50 @@ class AuthorSearchView(BrowserView):
         self.request.set('disable_border', True)
         return self.template(self)
 
+    def _render_cachekey(method, self):
+        preflang = getToolByName(self.context, 'portal_languages').getPreferredLanguage()
+        current_date = DateTime().Date()
+        return (preflang, current_date)
+
     @property
+    @ram.cache(_render_cachekey)
     def authors(self):
-        if not hasattr(self, '_authors'):
-            catalog = getToolByName(self.context, 'portal_catalog')
-            reviews = catalog({'fq': '+portal_type:(' + ' OR '.join(map(lambda x: '"%s"' % x, REVIEW_TYPES)) + ')',
-                               'facet': 'true',
-                               'facet.field': 'authors',
-                               'facet.limit': '-1',
-                               'facet.mincount': '1'}).facet_counts['facet_fields']['authors']
-            presentations = catalog({'fq': '+portal_type:(' + ' OR '.join(map(lambda x: '"%s"' % x, PRESENTATION_TYPES)) + ')',
-                                     'facet': 'true',
-                                     'facet.field': 'authors',
-                                     'facet.limit': '-1',
-                                     'facet.mincount': '1'}).facet_counts['facet_fields']['authors']
-            comments = catalog({'fq': '+portal_type:"Discussion Item"',
-                                'facet': 'true',
-                                'facet.field': 'authors',
-                                'facet.limit': '-1',
-                                'facet.mincount': '1'}).facet_counts['facet_fields']['authors']
+        catalog = getToolByName(self.context, 'portal_catalog')
+        reviews = catalog({'fq': '+portal_type:(' + ' OR '.join(map(lambda x: '"%s"' % x, REVIEW_TYPES)) + ')',
+                           'facet': 'true',
+                           'facet.field': 'authors',
+                           'facet.limit': '-1',
+                           'facet.mincount': '1'}).facet_counts['facet_fields']['authors']
+        presentations = catalog({'fq': '+portal_type:(' + ' OR '.join(map(lambda x: '"%s"' % x, PRESENTATION_TYPES)) + ')',
+                                 'facet': 'true',
+                                 'facet.field': 'authors',
+                                 'facet.limit': '-1',
+                                 'facet.mincount': '1'}).facet_counts['facet_fields']['authors']
+        comments = catalog({'fq': '+portal_type:"Discussion Item"',
+                            'facet': 'true',
+                            'facet.field': 'authors',
+                            'facet.limit': '-1',
+                            'facet.mincount': '1'}).facet_counts['facet_fields']['authors']
 
-            authors = self.request.get('authors')
-            if authors:
-                items = [x.lower() for x in authors.split(' ')]
-                self._authors = [dict(name=x, reviews=reviews.get(safe_unicode(x), 0), presentations=presentations.get(safe_unicode(x), 0), comments=comments.get(safe_unicode(x), 0)) for x in
-                    catalog.uniqueValuesFor('authors') if True in
-                    [y in x.lower() for y in items]]
-            else:
-                self._authors = [dict(name=x, reviews=reviews.get(safe_unicode(x), 0), presentations=presentations.get(safe_unicode(x), 0), comments=comments.get(safe_unicode(x), 0)) for x in
-                    catalog.uniqueValuesFor('authors')]
-            self._authors = filter(lambda x: x['presentations'] + x['reviews'] + x['comments'] != 0, self._authors)
+        authors = self.request.get('authors')
+        if authors:
+            items = [x.lower() for x in authors.split(' ')]
+            authors = [dict(name=x, reviews=reviews.get(safe_unicode(x), 0), presentations=presentations.get(safe_unicode(x), 0), comments=comments.get(safe_unicode(x), 0)) for x in
+                catalog.uniqueValuesFor('authors') if True in
+                [y in x.lower() for y in items]]
+        else:
+            authors = [dict(name=x, reviews=reviews.get(safe_unicode(x), 0), presentations=presentations.get(safe_unicode(x), 0), comments=comments.get(safe_unicode(x), 0)) for x in
+                catalog.uniqueValuesFor('authors')]
+        authors = filter(lambda x: x['presentations'] + x['reviews'] + x['comments'] != 0, authors)
 
-            self._alpha_index = {}
-            for letter in self.ALPHABET:
-                part = filter(lambda a: a['name'].startswith(letter), self._authors)
-                if part:
-                    self._alpha_index[letter] = self._authors.index(part[0])
-        return self._authors
+        return authors
 
     @property
+    @ram.cache(_render_cachekey)
     def alpha_index(self):
-        authors = self.authors
-        return self._alpha_index
+        alpha_index = {}
+        for letter in self.ALPHABET:
+            part = filter(lambda a: a['name'].startswith(letter), self.authors)
+            if part:
+                alpha_index[letter] = self.authors.index(part[0])
+        return alpha_index
