cfa54e3f57f939faee6bd03ef37de04a9318d28e
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2012-08-17 13:54:13 +0000

    Fix for #5293
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.theme/trunk@10155 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/theme/skins/recensio_theme/queryCatalogNoVolDoubles.py
+++ recensio/theme/skins/recensio_theme/queryCatalogNoVolDoubles.py
@@ -1,3 +1,12 @@
+## Script (Python) "queryCatalog"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=REQUEST=None,show_all=0,quote_logic=0,quote_logic_indexes=['SearchableText','Description','Title'],use_types_blacklist=False,show_inactive=False,use_navigation_root=False,b_start=None,b_size=None
+##title=wraps the portal_catalog with a rules qualified query
+##
 from ZODB.POSException import ConflictError
 from Products.ZCTextIndex.ParseTree import ParseError
 from Products.CMFCore.utils import getToolByName
