4a5e165af0d7673c29e383215dc9bf704ed2258b
Patrick Gerken <do3cc@patrick-gerken.de>
2012-08-23 18:26:26 +0200

    Clean up, and a good matcher

--- recensio/theme/skins/recensio_theme/javascript.js
+++ recensio/theme/skins/recensio_theme/javascript.js
@@ -1,32 +1,41 @@
-jq(document).ready(function() {
-    jq(".fieldErrorBox").each(function() {
-        if (this.textContent) { jq(this).addClass('highlighted'); }
+jq(document).ready(function () {
+    jq(".fieldErrorBox").each(function () {
+        if (this.textContent) {
+            jq(this).addClass('highlighted');
+        }
     });
-// hide_title is a style for the rich text editor.
-// This logic will always hide/unhide the next element
+    // hide_title is a style for the rich text editor.
+    // This logic will always hide/unhide the next element
     if (navigator.appName.search("Microsoft") !== -1) { // workaround for IE
         jq('p.hide_content').show();
     }
-    jq('h4.hide_title').click(function() {
+    jq('h4.hide_title').click(function () {
         jq(this).next().toggle();
     });
-    
+
 
     jq('.easyticker').easyticker({
         speed: 'slow',
         dureeAffichage: 7000
     });
 
-// This is a tiny bit hacky. BoolWidgets don't support 'required' property and 
-// don't show errors. We add the basics via jquery.
+    // This is a tiny bit hacky. BoolWidgets don't support 'required' property and 
+    // don't show errors. We add the basics via jquery.
     jq('#formfield-form-declaration_of_identity label').after('<span style="color: #f00;" title="Required" class="required"> ■ </span>')
     if (jq('dl.error').length > 0 && !jq('#formfield-form-declaration_of_identity input.checkboxType').attr('checked')) {
-            jq('#formfield-form-declaration_of_identity').addClass('error');
+        jq('#formfield-form-declaration_of_identity').addClass('error');
     }
 
-    if(jq("textarea#subject").length){
-        jq.getJSON("/subject_list_json", function(data){
-            jq("textarea#subject").typeahead({source: data});
+    if (jq("textarea#subject").length) {
+        jq.getJSON("/subject_list_json", function (data) {
+            jq("textarea#subject").typeahead({
+                source: data,
+                matcher: function(term){
+                    var entries = this.query.split('\n'),
+                        searching_for = entries[entries.length-1];
+                    return term.find(searching_for) !== -1;
+                }
+            });
         });
     }
 });
