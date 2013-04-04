329ad39d3ad58279c3e25203a620bb1ce6b2e93c
Patrick Gerken <do3cc@patrick-gerken.de>
2013-04-04 08:51:58 +0200

    Catch untranslated translations

--- recensio/theme/browser/homepage.py
+++ recensio/theme/browser/homepage.py
@@ -1,5 +1,6 @@
 from DateTime import DateTime
 
+from AccessControl import Unauthorized
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
@@ -198,7 +199,15 @@ class HomepageView(BrowserView):
                 (context, self.request), name=u'plone_portal_state')
 
             lang = portal_state.language()
-            pubs = [brain.getObject().restrictedTraverse(brain.getObject().getDefaultPage()).getTranslations()[lang][0] for brain in pc(query)]
+            pubs = []
+            for brain in pc(query):
+                try:
+                    obj = brain.getObject()
+                    default_page = obj.restrictedTraverse(obj.getDefaultPage())
+                    translated_ob = default_page.getTranslations()[lang][0]
+                    pubs.append(translated_ob)
+                except Unauthorized:
+                    continue
             items = [dict(title=x.Title(), url='/'+x.absolute_url(1)) for x in pubs]
             return sorted(items, key=lambda p: p['title'].lower())
         else:
