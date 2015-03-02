5043ad9b5b14f23d506343547442605a7638e6e4
Manuel Reinhardt <reinhardt@syslab.com>
2015-03-02 11:56:11 +0000

    don't sort pubs by id. 'pubs' actually contains the default pages of the publications, so sorting on their ids is probably counter productive (they could all be 'index_html', but don't have to be). The sort_on parameter in the query already does the trick. refs #11023

--- recensio/theme/browser/homepage.py
+++ recensio/theme/browser/homepage.py
@@ -210,8 +210,8 @@ class HomepageView(BrowserView):
                         pubs.append(translated_ob)
                 except Unauthorized:
                     continue
-            items = [dict(title=x.Title(), id=x.getId(), url='/'+x.absolute_url(1)) for x in pubs]
-            return sorted(items, key=lambda p: p['id'])
+            items = [dict(title=x.Title(), url='/'+x.absolute_url(1)) for x in pubs]
+            return items
         else:
             # This can only happen, when there is no initial content yet
             return []
