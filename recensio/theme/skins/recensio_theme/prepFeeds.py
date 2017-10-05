64d281a9cbd041be05e13cfc1659c88ed72a1f40
Manuel Reinhardt <reinhardt@syslab.com>
2017-10-05 11:44:55 +0000

    search_rss must be called on navigation root. Refs #15955

--- recensio/theme/skins/recensio_theme/prepFeeds.py
+++ recensio/theme/skins/recensio_theme/prepFeeds.py
@@ -19,7 +19,7 @@ zss = context.portal_catalog(
 feeds = []
 parms = {}
 
-tmpl = """search_rss?modified_usage=range%%3Amin&isbn=&series=&sort_order=descending&year=&path:list=%(path)s&sort_on=created&titleOrShortname=&publisher=&created:date:list=1970/02/01&b_size:int=100&Creator=&modified:date:list=1970/02/01&submit=Suche&SearchableText=&place=&authorsFulltext=&created_usage=range%%3Amin&recensio_id=&rss_title=%(rss_title)s"""
+tmpl = """../search_rss?modified_usage=range%%3Amin&isbn=&series=&sort_order=descending&year=&path:list=%(path)s&sort_on=created&titleOrShortname=&publisher=&created:date:list=1970/02/01&b_size:int=100&Creator=&modified:date:list=1970/02/01&submit=Suche&SearchableText=&place=&authorsFulltext=&created_usage=range%%3Amin&recensio_id=&rss_title=%(rss_title)s"""
 #tmpl = """search_rss?rss_title=%(rss_title)s&modified_usage=range%%3Amin&sort_order=descending&path:list=%(path)s&sort_on=created&created:date:list=1970/02/01&b_size:int=100&modified:date:list=1970/02/01&submit=Suche&created_usage=range%%3Amin"""
 
 for z in zss:
