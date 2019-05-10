d280960170a11aaddfe3f6b62d74bbbfbb4589b0
Manuel Reinhardt <reinhardt@syslab.com>
2019-05-10 11:04:39 +0200

    reindex_on_reorder: Don't fail completely if reindexing fails. Refs #FLOW-229

--- recensio/theme/eventhandlers.py
+++ recensio/theme/eventhandlers.py
@@ -1,6 +1,14 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
 def reindex_on_reorder(obj, evt):
     """ Quick fix: for some reason objects are not reindexed properly after
     reordering. Needed for FLOW-229
     """
     for child in obj.objectValues():
-        child.reindexObject()
+        try:
+            child.reindexObject()
+        except Exception:
+            log.info('Not reindexing {}'.format(child.getId()))
