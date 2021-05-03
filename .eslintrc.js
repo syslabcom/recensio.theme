ec6a076f2ff4e1e2524d0db73fd502eab3869e46
Johannes Raggam <thetetet@gmail.com>
2021-05-03 09:52:19 +0200

    scr-1050 - Add code format/QA tool configs.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    extends: ["eslint:recommended", "prettier"],
+    root: true,
+    env: {
+        es6: "false",
+        browser: true,
+    },
+    rules: {
+        "no-debugger": 1,
+        "no-duplicate-imports": 1,
+        // Do keep due avoid unintendet consequences.
+        "no-alert": 0,
+        "no-control-regex": 0,
+        "no-self-assign": 0,
+        "no-useless-escape": 0,
+    },
+};
