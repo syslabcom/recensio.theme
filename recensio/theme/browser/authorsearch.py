dc3e8c85016938a9ab96afa5698e9981b405a9c4
Manuel Reinhardt <reinhardt@syslab.com>
2021-10-25 09:41:45 +0200

    Further authorsearch fixes and optimization. Refs SCR-1422

--- recensio/theme/browser/authorsearch.py
+++ recensio/theme/browser/authorsearch.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+from datetime import datetime
 from plone.memoize import instance
+from plone.memoize import ram
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import safe_unicode
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from recensio.theme.browser.views import CrossPlatformMixin
+
+
+def _render_cachekey(method, self):
+    hour = datetime.now().strftime("%Y-%m-%d:%H")
+    b_start = self.request.get("b_start", "0")
+    letter = self.request.get("letter")
+    return (b_start, letter, hour)
 
 
 class AuthorSearchView(BrowserView, CrossPlatformMixin):
@@ -47,6 +55,7 @@ class AuthorSearchView(BrowserView, CrossPlatformMixin):
         self.request.set("disable_border", True)
         return self.template(self)
 
+    @ram.cache(_render_cachekey)
     def get_b_start(self):
         b_start = int(self.request.get("b_start", 0))
         if b_start:
@@ -65,25 +74,33 @@ class AuthorSearchView(BrowserView, CrossPlatformMixin):
                 "fl": "Title",
             }
             num_authors = len(catalog(query))
-            b_size = 2000
-            b_start = num_authors / 2 - b_size / 2
+            if num_authors == 0:
+                return 0
+            b_size = 30
+            query["b_size"] = b_size
+            partition = [0, num_authors]
             while True:
-                query.update({
-                    "b_start": b_start,
-                    "b_size": b_size,
-                })
-                partition = catalog(query)
+                b_start = max(
+                    partition[0] + (partition[1] - partition[0]) / 2 - b_size / 2,
+                    0,
+                )
+                query["b_start"] = b_start
+                sample = catalog(query)
                 last_index = min(b_start + b_size - 1, num_authors - 1)
-                if partition[last_index]["Title"][0].lower() < letter:
-                    b_start = min(b_start + (num_authors - b_start) / 2, num_authors)
-                elif partition[b_start]["Title"][0].lower() > letter:
-                    b_start = max(b_start - (num_authors - b_start) / 2, 0)
-                else:
+                if sample[last_index]["Title"][0].lower() < letter:
+                    partition[0] = min(last_index, num_authors)
+                elif sample[b_start]["Title"][0].lower() < letter or b_start == 0:
                     break
+                elif sample[b_start]["Title"][0].lower() == letter:
+                    partition[1] = max(b_start + 2, 0)
+                else:
+                    partition[1] = max(b_start, 0)
             for idx in range(b_start, last_index):
-                if partition[idx]["Title"].lower().startswith(letter):
+                if sample[idx]["Title"].lower().startswith(letter):
                     b_start = idx
                     break
+            b_start = int(b_start / 30) * 30
+        print(b_start)
         return b_start
 
     @property
